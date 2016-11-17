03af5150c76589e998a9770aa8be2e6b70dd5b38
Henning Dieterichs <henning.dieterichs@hediet.de>
2016-11-17 23:03:13 +0100

    Added support for mobx and scss.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,33 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import "./style.css";
+import * as classNames from "classnames";
+import { observable, computed } from "mobx";
+import { observer } from "mobx-react";
+import DevTools from 'mobx-react-devtools'; 
+import "./style.scss";
 
+class Model {
+	constructor() {
+		this.time = new Date();
+		setInterval(() => this.time = new Date(), 1000);
+	}
+
+	@observable time: Date;
+	@computed get seconds(): number {
+		return this.time.getSeconds();
+	}
+}
+
+var model = new Model();
+
+@observer
 class GUI extends React.Component<{}, {}> {
 	render() {
 		return (
 			<div>
-				hello world
+				<DevTools />
+				{model.time.toDateString()},
+				second: {model.seconds}
 			</div>
 		);
 	}
